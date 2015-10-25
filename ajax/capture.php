fbeb25a8a19f49e3997d3079155bdce6106958da
bethory <lic.vizcaya@gmail.com>
2015-10-25 09:24:33 -0500

    archivos base

---
+++ ajax/capture.php
@@ -0,0 +1,9 @@
+<?php
+    require "../conexion.php";
+    $query=mysql_query("
+        SELECT *
+        FROM `Provincias`
+        WHERE `Nombre` LIKE '" . mysql_escape_string($_POST['prueba']) . "'
+    ");
+    echo (mysql_num_rows($query)!==0) ? mysql_result($query, 0, idProvincias) : "no encontrada";
+?>
