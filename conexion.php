20324d6ec40611e4a9faa2d295ae8ce29e84b6d8
devllc <dev@llc>
2015-10-26 17:57:18 -0500

    usuario y contraseña

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?php
     //Conexión a la base de datos
-    $con=mysql_connect("diegoquiroga.com", "diegoqui_usuario", "proyecto212");
+    $con=mysql_connect("", "", "");
     mysql_set_charset('utf8');
-    mysql_select_db("diegoqui_convenio212", $con) or die("conexión no establecida");
+    mysql_select_db("", $con) or die("conexión no establecida");
 ?>
